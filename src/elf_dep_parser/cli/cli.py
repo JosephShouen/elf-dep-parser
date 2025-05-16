9a23f6b3ae984fc7590fa5682aeefedc78de6024
JosephShouen <ggg0605@yandex.ru>
2025-05-16 15:41:28 +0300

    requirments, toml, cli app

--- src/elf_dep_parser/cli/cli.py
+++ src/elf_dep_parser/cli/cli.py
@@ -1,1 +1,8 @@
+# import elf_dep_parser.elf_dep_parser as parser
+from elf_dep_parser.parser import get_elf_dependencies
 
+def main():
+    print("Hello Parser")
+
+if __name__ == "__main__":
+    main()
